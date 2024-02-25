69cbf676b66f89259959914b3d1831fa105934be
Daniel Neel <dneel10@gmail.com>
2024-02-25 00:52:38 -0500

    add user model with devise, begin adding event/user associations and controller actions

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,14 @@
+class EventsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+  def index
+    @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.create_event(event_params)
+  end
+end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,4 @@
 class Event < ApplicationRecord
+  belongs_to :creator, class_name: :user, foreign_key: :user_id
+  has_many :attendees, c: :user
 end
