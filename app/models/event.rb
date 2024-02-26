e71074d81269c8f0e8d1fccfd51f08c244b21893
Danny <dneel10@gmail.com>
2024-02-26 15:26:36 -0500

    update to add inverse_of tags

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ApplicationRecord
   validates :title, presence: true
   validates :date, presence: true
 
-  belongs_to :creator, class_name: "User", foreign_key: :creator_id, inverse_of: :created_events
+  belongs_to :creator, class_name: "User", inverse_of: :created_events
 
   has_many :attendances, foreign_key: :attended_event_id, dependent: :destroy
   has_many :attendees, through: :attendances, source: :attendee
