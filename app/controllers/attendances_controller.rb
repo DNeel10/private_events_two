6c65c333220616842184f180e7f587ebf3fda6d3
Danny <dneel10@gmail.com>
2024-02-26 19:35:19 -0500

    update to allow users to add themselves as an attendee, and remove the attendance from the user#show page

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -4,9 +4,20 @@ class AttendancesController < ApplicationController
   def create
     @event = Event.find(params[:attendances][:attended_event_id])
     @attendance = current_user.attendances.create(attendance_params)
+
+    if @attendance.save
+      redirect_to @event
+    else
+      flash.now[:notice] = "You were not added to the attendee list"
+      render 'events/show', status: :unprocessable_entity
+    end
   end
 
-  def delete
+  def destroy
+    @attendance = Attendance.find(params[:id])
+    @attendance.destroy
+
+    redirect_to current_user
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :created_events, foreign_key: :creator_id, class_name: "Event"
+  has_many :created_events, foreign_key: :creator_id, class_name: "Event", inverse_of: :creator
   
   has_many :attendances, foreign_key: :attendee_id, dependent: :destroy
   has_many :attended_events, through: :attendances
